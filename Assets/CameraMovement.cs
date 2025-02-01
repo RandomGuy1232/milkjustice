7a0db05cfbc4a77c0d0138a98989747ec273cad3
RandomGuy1232 <endertazbaz@gmail.com>
2025-02-01 14:38:08 -0800

    Add files via upload

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+    void Update() {
+        transform.position = player.transform.position + offset;
+    }
+}
